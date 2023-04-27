962ae097d95830fab0053b31769820bee26e3875
mertnamsal <namsalmert@gmail.com>
2023-04-27 22:40:42 +0300

    sidebar-bitti

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,66 +1,42 @@
 import React from "react";
 import './sidebar.scss'
+import { Link } from "react-router-dom"
+import HowToRegOutlinedIcon from '@mui/icons-material/HowToRegOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import SupervisorAccountOutlinedIcon from '@mui/icons-material/SupervisorAccountOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+import EngineeringOutlinedIcon from '@mui/icons-material/EngineeringOutlined';
+import Groups3Icon from '@mui/icons-material/Groups3';
 const Sidebar = () => {
+  
   return (
-    <aside className="main-sidebar sidebar-dark-primary elevation-4">
-      <a href="" className="brand-link">
-        <span className="brand-text font-weight-light">HumanResources</span>
-      </a>
-      <div className="sidebar">
-        <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div className="image">
-            <img
-              src="https://images.unsplash.com/photo-1682478695074-9e47f09a7459?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-              className="img-circle elevation-2"
-              alt="User Image"
-            />
-          </div>
-          <div className="info">
-            <a href="#" className="d-block">
-              Mahmut Tuncer
-            </a>
-          </div>
+    <aside className="full-sidebar">
+      <div>
+      <div className="sidebar-top">
+        <Link className="sidebar-title-link">
+          <img src="https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" alt="" />
+          <p className="sidebar-title">HR Admin Page</p>
+        </Link>
+        
+        <div className="sidebar-admin-panel">
+          <Link className="sidebar-admin-panel-link">
+          <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="admin-image" />
+          <p className="admin-name">Mahmut</p>
+          </Link>
         </div>
-        <nav className="mt-2">
-          <ul
-            className="nav nav-pills nav-sidebar flex-column"
-            data-widget="treeview"
-            role="menu"
-            data-accordion="false"
-          >
-           
-            <li className="nav-item">
-              <a href="" className="nav-link">
-                <i className="nav-icon fas fa-th"></i>
-                <p>
-                  Kullanıcı Kayıt
-                </p>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                <i className="nav-icon fas fa-copy"></i>
-                <p>
-                  Profil
-                  <i className="fas fa-angle-left right"></i>
-                  
-                </p>
-              </a>
-             
-            </li>
-            <li className="nav-item">
-              <a href="#" className="nav-link">
-                <i className="nav-icon fas fa-copy"></i>
-                <p>
-                  Kullanıcıları Listele
-                  <i className="fas fa-angle-left right"></i>
-                </p>
-              </a>
-             
-            </li>
-            
-          </ul>
-        </nav>
+      </div>
+      <div className="sidebar-main">
+        <ul className="sidebar-nav">
+        <li><Link className="link d-flex"><AccountCircleOutlinedIcon/><div>My Profile</div></Link></li>
+          <li><Link className="link d-flex"> <HowToRegOutlinedIcon/><div>Register</div></Link></li>
+          <li><Link className="link d-flex"><SupervisorAccountOutlinedIcon/><div>Admin List</div></Link></li>
+          <li><Link className="link d-flex"><Groups3Icon/><div>Project Manager List</div></Link></li>
+          <li><Link className="link d-flex"><EngineeringOutlinedIcon/><div>Worker List</div></Link></li>
+        </ul>
+      </div>
+      </div>
+      <div className="logout d-flex">
+      <LogoutOutlinedIcon/><div className="fw-large lout">Logout</div>
       </div>
     </aside>
   );
