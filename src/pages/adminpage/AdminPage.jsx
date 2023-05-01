ef7752f130b94bacc3bf88d31e435a006992d2bb
busecnky <cankayabuse94@gmail.com>
2023-05-02 01:47:28 +0300

    first commit

--- src/pages/adminpage/AdminPage.jsx
+++ src/pages/adminpage/AdminPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import Sidebar from "../../components/sidebar/Sidebar";
+import MainPage from "../../components/mainpage/MainPage";
 import "./adminpage.scss";
 
 const AdminPage = () => {
@@ -10,6 +11,8 @@ const AdminPage = () => {
         <Sidebar />
       </div>
       <Header />
+      <MainPage/>
+      <p>Hello</p>
     </div>
   );
 };
