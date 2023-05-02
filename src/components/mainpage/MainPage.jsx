d7dfc8c05c078d177037cf8963916c2aa4bf670c
busecnky <cankayabuse94@gmail.com>
2023-05-02 13:27:56 +0300

    merge with mertnamsal

--- src/components/mainpage/MainPage.jsx
+++ src/components/mainpage/MainPage.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import "./mainpage.scss";
 
 const MainPage = () => {
   return (
-    <div>MainPage</div>
-  )
-}
+    <div className="maincontainer">
+        <div className="card card-widget widget-user">
+          <div className="widget-user-header bg-info">
+            <h3 className="widget-user-username">Alexander Pierce</h3>
+            <h5 className="widget-user-desc">Founder & CEO</h5>
+          </div>
+          <div className="widget-user-image">
+            <img
+              className="img-circle elevation-2"
+              src="https://images.pexels.com/photos/997512/pexels-photo-997512.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="User Avatar"
+            />
+          </div>
+          <div className="card-footer">
+            <div className="row">
+              <div className="col-sm-4 border-right">
+                <div className="description-block">
+                  <h5 className="description-header">3,200</h5>
+                  <span className="description-text">SALES</span>
+                </div>
+              </div>
+              <div className="col-sm-4 border-right">
+                <div className="description-block">
+                  <h5 className="description-header">13,000</h5>
+                  <span className="description-text">FOLLOWERS</span>
+                </div>
+              </div>
+              <div className="col-sm-4">
+                <div className="description-block">
+                  <h5 className="description-header">35</h5>
+                  <span className="description-text">PRODUCTS</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+};
 
-export default MainPage
+export default MainPage;
--- src/pages/admin-page/AdminPage.jsx
+++ src/pages/admin-page/AdminPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import Sidebar from "../../components/sidebar/Sidebar";
+import MainPage from "../../components/mainpage/MainPage";
 import "./adminpage.scss";
 
 const AdminPage = () => {
@@ -9,8 +10,11 @@ const AdminPage = () => {
       <div className="admin_sidebar">
         <Sidebar />
       </div>
+      <div className="admin-right">
         <Header /> 
-        
+        <MainPage/>
+      </div>
+
     </div>
   );
 };
