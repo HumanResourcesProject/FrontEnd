b0cf3777bf438e69fb355caeb1b93a9402f6c510
MFurkanTurkmen <f.turkmen06@gmail.com>
2023-05-04 15:40:47 +0300

    bos deneme

--- src/service/AdminService.js
+++ src/service/AdminService.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const FINDALL_ADMIN = "https://jsonplaceholder.typicode.com/albums/1/photos";
 // http://localhost:7070:/api/v1/admin/findall
 
+
 const CREATE_ADMIN = 'http://localhost:7070/admin/createadmin'
 class AdminService{
     getAllAdmins() {
