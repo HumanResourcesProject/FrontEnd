c1ecbcbc63f0df03cd68aa9340e5d9f7976316f5
oguz11 <tasginoguz@gmail.com>
2023-05-10 16:57:39 +0300

    CompanyMS-getAllCompany-eksik

--- src/components/companyManagerCreate/CompanyManagerCreate.jsx
+++ src/components/companyManagerCreate/CompanyManagerCreate.jsx
@@ -5,14 +5,7 @@ import DriveFolderUploadIcon from '@mui/icons-material/DriveFolderUpload';
 import "./companyManagerCreate.scss";
 
 const CompanyManagerCreate = () => {
-
-  const [image,setImage] = useState('');
-  const onchangeImage = (e) => {
-    const file = e.target.files[0];
-    setImage(file);
-  }
-
-  const [companies, setCompanies] = useState([]);
+ 
   const [managerInfo, setManagerInfo] = useState({
     identifynumber:"",
     avatar:"",
@@ -28,19 +21,46 @@ const CompanyManagerCreate = () => {
     phone:"",
     address:"",
   })
-  // bir seyler yazdım buraya
-  useEffect(() => {
-    axios.get("https://example.com/api/findallcompanies").then((response) => {
-      setCompanies(response.data);
-    });
-  }, []);
 
-  const handleInputChange = (event) => {
-    const { fieldName, fieldValue } = event.target;
-    setManagerInfo({
-      ...managerInfo,
-      [fieldName]: fieldValue,
-    });
+  // For Image ********
+  const [image,setImage] = useState('');
+  const onchangeImage = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+      }
+  //********* */
+  //  burada CompanyMS' deki endpoint' e istek atılacak.
+  // // Section where the CompanyList come :) ********
+  // const [companies, setCompanies] = useState([]);
+  // useEffect(() => {
+  //   axios.get("http://localhost:7071/company/getAllCompany").then((response) => {
+  //     setCompanies(response.data);
+  //   });
+  // }, []);
+  // //*************** */
+ 
+  const handleCreate = async (event) => {
+
+ 
+    event.preventDefault();
+
+    if(image === "https://cdn.pixabay.com/photo/2017/11/10/04/47/user-2935373_960_720.png"){
+      const newImage = URL.createObjectURL(image)
+        setManagerInfo({
+          ...managerInfo,
+          avatar: newImage
+        })
+    }
+
+      CompanyManagerService.createCompanyManager(managerInfo).then(
+        () =>{
+          
+          alert("added successfully *****")
+        })       
+        .catch((error) => {
+          alert(error.response.data.message+"isterler: password 3'den asaği olmucak ... mail: gmail.com ile bitecek.... telefon sadece numara olmalı... hicbir alan bos gecilemez");
+        });
+    
   };
 
   return (
@@ -60,10 +80,15 @@ const CompanyManagerCreate = () => {
       <div className='manager-register-profil-info'>
         <div className='manager-register-information'>
           <h2>Bilgi Alma Bölümü</h2>
-            <form onSubmit={handleInputChange}>
+            <form onSubmit={handleCreate}>
             <div className="manager-company-section">
               <label for="company-select">Select Company:</label>
-              <select id="company-select">
+              <select id="company-select"
+              onChange={(event) => 
+                setManagerInfo({...managerInfo, 
+                  companyName: event.target.value})}
+              >
+                {/* Burada companyMs nin endpoint' inden gelen Companyler listelenecek */}
                 <option value="">Please select a company</option>
                 {companies.map((company) => (
                   <option key={company.id} value={company.id}>
@@ -76,73 +101,113 @@ const CompanyManagerCreate = () => {
             <div className='manager-name-section'>
             <label for="id-number">Identify number:</label>
               <input
-               type="text"
-               id="id-number"
-               name="id-number"
+               type="text" onChange={(e) =>
+                setManagerInfo({
+                  ...managerInfo,
+                  identifynumber: e.target.value,
+                })
+              } 
                />
                <br />
               <label for="first-name">Name:</label>
               <input
-               type="text" 
-               id="first-name" 
-               name="first-name"
+               type="text" onChange={(e) =>
+                setManagerInfo({
+                  ...managerInfo,
+                  name: e.target.value,
+                })
+              }
                />
               <label for="last-name">Surname:</label>
               <input 
-              type="text" 
-              id="last-name" 
-              name="last-name"
+               type="text" onChange={(e) =>
+                setManagerInfo({
+                  ...managerInfo,
+                  surname: e.target.value,
+                })
+              }
               />
             </div>
             <br />
             <div className="manager-birth-section">
               <label for="birth-date">Birth-date:</label>
               <input
-               type="date" 
-               id="birth-date" 
-               name="birth-date"
+                type="text" onChange={(e) =>
+                  setManagerInfo({
+                    ...managerInfo,
+                    birthdate: e.target.value,
+                  })
+                }
                /> <br />
               <label for="birth-place">Birth-place:</label>
-              <input type="text" 
-              id="birth-place" 
-              name="birth-place"
+              <input 
+                type="text" onChange={(e) =>
+                  setManagerInfo({
+                    ...managerInfo,
+                    birthplace: e.target.value,
+                  })
+                }
               />
             </div>
             <br />
             <div className="manager-job-section">
               <label for="job-start-date">Job-start-date:</label>
-              <input type="date" 
-              id="job-start-date" 
-              name="job-start-date"
+              <input 
+                type="text" onChange={(e) =>
+                  setManagerInfo({
+                    ...managerInfo,
+                    hiredate: e.target.value,
+                  })
+                }
               /><br />
               <label for="job-title">Occupation:</label>
-              <input type="text" 
-              id="job-title" 
-              name="job-title"
+              <input 
+                type="text" onChange={(e) =>
+                  setManagerInfo({
+                    ...managerInfo,
+                    jobtitle: e.target.value,
+                  })
+                }
               /><br />
               <label for="department">Department:</label>
-              <input type="text" 
-              id="department" 
-              name="department"
+              <input 
+                type="text" onChange={(e) =>
+                  setManagerInfo({
+                    ...managerInfo,
+                    birthdate: e.target.value,
+                  })
+                }
               />
             </div>
             <br />
             <div className="manager-com-section">
               <label for="email">E-Mail</label>
-              <input type="email" 
-              id="email" 
-              name="email"
+              <input 
+                type="text" onChange={(e) =>
+                  setManagerInfo({
+                    ...managerInfo,
+                    email: e.target.value,
+                  })
+                }
               />
               <label for="phone">Phone:</label>
-              <input type="tel" 
-              id="phone" 
-              name="phone"
+              <input 
+                type="text" onChange={(e) =>
+                  setManagerInfo({
+                    ...managerInfo,
+                    phone: e.target.value,
+                  })
+                }
               />
               <br />
               <label for="address">Address:</label>
-              <input type="text" 
-              id="address" 
-              name="address"
+              <input 
+                type="text" onChange={(e) =>
+                  setManagerInfo({
+                    ...managerInfo,
+                    address: e.target.value,
+                  })
+                }
               />
             </div>
             <button type="submit" class="save-button">Save</button>
