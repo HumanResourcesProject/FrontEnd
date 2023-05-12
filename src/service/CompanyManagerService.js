cf4410bcec789eb36e7be36672c2401fa60c99e3
busecnky <cankayabuse94@gmail.com>
2023-05-12 05:44:38 +0300

    manager update profile

--- src/service/CompanyManagerService.js
+++ src/service/CompanyManagerService.js
@@ -4,6 +4,11 @@ const FINDALL_MANAGER = "http://localhost:7072/companymanager/findall";
 const CREATE_MANAGER = "http://localhost:7070/admin/createcompanymanager";
 const CREATE_EMPLOYEE = "http://localhost:7070/admin/createemployee";
 const SHORT_DETAIL = 'http://localhost:7072/companymanager/getshortdetail';
+const GET_MANAGER_INFO = "http://localhost:7072/companymanager/getmanager";
+const UPDATE_MANAGER_INFO = "http://localhost:7072/companymanager/updatecompanymanager";
+const UPDATE_PROFILEP = "http://localhost:7072/companymanager/imagescloud";
+
+
 
 
 class CompanyManagerService {
@@ -31,7 +36,25 @@ class CompanyManagerService {
     postShortDetails(token){
         return axios.post(SHORT_DETAIL,token);
     }
+    getManagerInformations(token){
+        return axios.post(GET_MANAGER_INFO,token,{
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+    updateManagerInfo(data){
+        return axios
+        .put(UPDATE_MANAGER_INFO, data, {
+          headers: {
+            'Content-Type': 'application/json'
+        }
+        });
 
+    }
+    getManagerProfilePhoto(formData){
+        return axios.post(UPDATE_PROFILEP, formData)
+    }
 }
 export default new CompanyManagerService();
 
