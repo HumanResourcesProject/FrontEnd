02affd99fc0cbe1f354ef40d20f83e7c9de03103
mertnamsal <namsalmert@gmail.com>
2023-05-22 12:14:16 +0300

    a

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -26,7 +26,6 @@ const Header = ({ onToggleSidebar }) => {
     <div className="navbar">
       <div className="navbarleftside">
         <MenuIcon onClick={onToggleSidebar} className="menuicon" />
-        <p>Home</p>
       </div>
       <div className="navbarrightside">
         <SearchOutlinedIcon className="searchicon" />
