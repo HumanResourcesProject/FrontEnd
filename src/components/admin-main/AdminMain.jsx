0bd3fc2fdccf38856318cf118eee28c258b47bea
busecnky <cankayabuse94@gmail.com>
2023-05-23 23:31:31 +0300

    main

--- src/components/admin-main/AdminMain.jsx
+++ src/components/admin-main/AdminMain.jsx
@@ -1,14 +1,5 @@
 import React from 'react'
-import {
-  MDBCard,
-  MDBCardImage,
-  MDBCardBody,
-  MDBCardTitle,
-  MDBCardText,
-  MDBCardFooter,
-  MDBRow,
-  MDBCol
-} from 'mdb-react-ui-kit';
+
 
  const AdminMain = () => {
   return (
