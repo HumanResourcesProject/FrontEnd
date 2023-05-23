8e83c6fc7abc37fae105f3c179ab5cb028d85de9
busecnky <cankayabuse94@gmail.com>
2023-05-24 00:25:12 +0300

    admin main cards

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import Box from "@mui/material/Box";
-import Popper from "@mui/material/Popper";
-import Fade from "@mui/material/Fade";
 import { unstable_styleFunctionSx } from '@mui/system';
 import { createTheme } from '@mui/material/styles';
 import "./card.scss";
@@ -9,56 +6,35 @@ import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 import DeleteIcon from "@mui/icons-material/Delete";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import styled from "@emotion/styled";
-import CompanyService from "../../service/CompanyService";
 
-const Card = () => {
-
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const token = {
-      token: sessionStorage.getItem("token"),
-      role: sessionStorage.getItem("role"),
-    };
-
-    CompanyService.findAllCompany(token)
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data: ", error);
-      });
-  }, []);
+const Card = ({company}) => {
 
 
   return (
     <div className="widget-user-mini">
       <div className="widget-user-header-mini ">
-        <h3 className="widget-user-username-mini">{userData.name}</h3>
-        <h5 className="widget-user-desc-mini">Founder & CEO</h5>
+        <h3 className="widget-user-username-mini"></h3>
+        <h5 className="widget-user-desc-mini">{company.name}</h5>
       </div>
       <div className="img-cropper-mini">
         <img
           className="img-circle-mini"
-          src="https://images.pexels.com/photos/997512/pexels-photo-997512.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            src={company.logo}
           alt="User Avatar"
         />
       </div>
       <div className="card-footer-mini">
         <div className="card-footer-row-mini">
           <div className="description-block-mini">
-            <MailOutlineIcon
-              className="mailicon cardicons"
-            />
+          {company.phone}
           </div>
 
           <div className="description-block-mini border-leftright-mini">
-            <ManageAccountsIcon className="manageicon cardicons" />
+          {company.email}
           </div>
 
           <div className="description-block-mini">
-            <DeleteIcon className="deleteicon cardicons" />
-          </div>
+          {company.address}          </div>
         </div>
       </div>
     </div>
