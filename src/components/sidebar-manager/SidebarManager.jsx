967b0452d9e1675f6e874dbc1933ef6fa42ab981
mertnamsal <namsalmert@gmail.com>
2023-06-06 20:08:17 +0300

    fix

--- src/components/sidebar-manager/SidebarManager.jsx
+++ src/components/sidebar-manager/SidebarManager.jsx
@@ -6,15 +6,44 @@ import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import letterm from "../../assets/images/letter-m.png";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import GroupsIcon from "@mui/icons-material/Groups";
-import DescriptionIcon from "@mui/icons-material/Description";
+
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const SidebarManager = () => {
   const location = useLocation();
   const [activeItem, setActiveItem] = useState("");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isPage1Open, setIsPage1Open] = useState(false);
+  const [isPage2Open, setIsPage2Open] = useState(false);
+  const [isPage3Open, setIsPage3Open] = useState(false);
+  const [isDropdownHovered, setIsDropdownHovered] = useState(false);
 
   useEffect(() => {
     setActiveItem(location.pathname);
   }, [location]);
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const togglePage1 = () => {
+    setIsPage1Open(!isPage1Open);
+  };
+
+  const togglePage2 = () => {
+    setIsPage2Open(!isPage2Open);
+  };
+
+  const togglePage3 = () => {
+    setIsPage3Open(!isPage3Open);
+  };
+  const handleDropdownMouseEnter = () => {
+    setIsDropdownHovered(true);
+  };
+
+  const handleDropdownMouseLeave = () => {
+    setIsDropdownHovered(false);
+  };
 
   return (
     <aside className="full-sidebar-manager">
@@ -78,10 +107,52 @@ const SidebarManager = () => {
                 className={`link d-flex ${
                   activeItem === "/managerequests" ? "active" : ""
                 }`}
+                onClick={toggleDropdown}
               >
-                <DescriptionIcon />
+                {isDropdownOpen && <KeyboardArrowUpIcon />}
+                {!isDropdownOpen && <KeyboardArrowDownIcon />}
                 <div>Manage Requests</div>
               </Link>
+              {isDropdownOpen || isDropdownHovered ? (
+                <ul
+                  className="dropdown-menu"
+                  onMouseEnter={handleDropdownMouseEnter}
+                  onMouseLeave={handleDropdownMouseLeave}
+                >
+                  <li onClick={togglePage1}>
+                    <Link
+                      to="listemployeeleaves"
+                      className={`link2 ${
+                        activeItem === "/listemployeeleaves" ? "active" : ""
+                      }`}
+                    >
+                      Leave
+                    </Link>
+                  </li>
+                  <li onClick={togglePage2}>
+                    <Link
+                      to="listemployeeadvancepayments"
+                      className={`link2 ${
+                        activeItem === "/listemployeeadvancepayments"
+                          ? "active"
+                          : ""
+                      }`}
+                    >
+                      Advance Payment
+                    </Link>
+                  </li>
+                  <li onClick={togglePage3}>
+                    <Link
+                      to="listemployeeexpenses"
+                      className={`link2 ${
+                        activeItem === "/listemployeeexpenses" ? "active" : ""
+                      }`}
+                    >
+                      Expense
+                    </Link>
+                  </li>
+                </ul>
+              ) : null}
             </li>
           </ul>
         </div>
